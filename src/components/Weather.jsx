bae7c36712de85e6bfc75b0f81e8805b92b087e6
ritesh <riteshchaudhary375@gmail.com>
2024-07-16 16:32:04 +0545

    adding new ui for the app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
+import location_icon from "../assets/location.png";
 import clear_icon from "../assets/clear.png";
 import cloud_icon from "../assets/cloud.png";
 import drizzle_icon from "../assets/drizzle.png";
@@ -43,7 +44,7 @@ const Weather = () => {
 
       const res = await fetch(url);
       const data = await res.json();
-      //   console.log(data);
+      console.log(data);
 
       if (!res.ok) {
         alert(data.message);
@@ -61,7 +62,7 @@ const Weather = () => {
       });
     } catch (error) {
       setWeatherData(false);
-      console.log(error);
+      console.log(error.message);
     }
   };
 
@@ -78,6 +79,7 @@ const Weather = () => {
           alt="search"
           onClick={() => search(inputRef.current.value)}
         />
+        <img src={location_icon} alt="location" />
       </div>
 
       {weatherData ? (
