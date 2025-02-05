66ec371f86d6219f8e99095dfac5c9c2ea1d72e3
ritesh <riteshchaudhary375@gmail.com>
2025-02-05 17:57:30 +0545

    update show hide button for forecast data

--- src/components/ForecastAccordianButton.jsx
+++ src/components/ForecastAccordianButton.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { MdAdd } from "react-icons/md";
 import { RiSubtractFill } from "react-icons/ri";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const ForecastAccordianButton = ({ showForecast, setShowForecast }) => {
   return (
     <div className="forecast-accordian">
       <div className="content">
         <p>Forecast</p>
-        <div className="forecast-button" onClick={() => setShowForecast(!showForecast)}>
-          {!showForecast ? <MdAdd size={25} /> : <RiSubtractFill />}
+        <div
+          className="forecast-button"
+          onClick={() => setShowForecast(!showForecast)}
+        >
+          {!showForecast ? (
+            <span className="showForecastData">
+              Show
+              <IoIosArrowDown size={25} />
+            </span>
+          ) : (
+            <span className="showForecastData">
+              Hide
+              <IoIosArrowUp size={25} />
+            </span>
+          )}
         </div>
       </div>
       <hr />
